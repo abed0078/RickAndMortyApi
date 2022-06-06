89ce6eab6fab0c423ec341d782a6bf0da9c3198b
abed007 <abousy-gg@outlook.com>
2022-06-06 13:29:08 +0300

    RickAndMortyApi

--- app/src/main/java/com/example/rickandmorty/SharedRepository.kt
+++ app/src/main/java/com/example/rickandmorty/SharedRepository.kt
@@ -6,11 +6,13 @@ import retrofit2.http.Path
 class SharedRepository {
     suspend fun getCharacterById(characterId:Int): GetCharacterByIdResponse?{
          val request=NetworkLayer.apiClient.getCharacterById(characterId)
-
-        if(request.isSuccessful){
-            return request.body()!!
+        if(request.failed){
+            return null
         }
-        return null
+        if(!request.isSuccessful){
+            return null
+        }
+        return request.body
     }
 
 }
